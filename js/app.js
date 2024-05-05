943efba75b9a1aafd998d736194b11ea383998a4
Furkan Umut Ç. <umutcomalak@gmail.com>
2024-05-05 23:59:58 +0300

    commit
    commit

--- js/app.js
+++ js/app.js
@@ -1,19 +1,52 @@
 class CalorieTracker {
   constructor() {
-    this._calorieLimit = 2000;
+    this._calorieLimit = 2200;
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+
+    this._displayCaloriesLimit();
+    this._displayCaloriesTotal();
   }
+
+  // Public Methods/API //
 
   addMeal(meal) {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+    this._render();
   }
 
   addWorkout(workout) {
     this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+    this._render();
+  }
+
+  // Private Methods //
+
+  _displayCaloriesTotal() {
+    const totalCaloriesEl = document.getElementById("calories-total");
+    totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+
+  _displayCaloriesLimit() {
+    const calorieLimitEl = document.getElementById("calories-limit");
+    calorieLimitEl.innerHTML = this._calorieLimit;
+  }
+
+  _displayCaloriesConsumed() {
+    const caloriesConsumedEl = document.getElementById("calories-consumed");
+    const consumed = this._meals.reduce(
+      (total, meal) => total + meal.calories,
+      0
+    );
+
+    caloriesConsumedEl.innerHTML = consumed;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
   }
 }
 
@@ -36,7 +69,9 @@ class Workout {
 const tracker = new CalorieTracker();
 
 const breakfast = new Meal("breakfast", 400);
+const launch = new Meal("launch", 450);
 tracker.addMeal(breakfast);
+tracker.addMeal(launch);
 
 const run = new Workout("Run", 300);
 tracker.addWorkout(run);
